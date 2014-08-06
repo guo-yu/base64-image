1ccb9a8f36d55cab05665c926d8863583c6bc46f
Guo Yu <i@guoyu.me>
2014-08-06 17:24:26 +0800

    0.1: format codes, ready for shipping

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-var fs = require('fs'),
-    path = require('path');
+var fs = require('fs');
+var path = require('path');
 
-var checkBase64 = function(raw) {
-    return raw && typeof(raw) == 'string' && raw.match(/^data:image\/png;base64,/)
+module.exports = base64ToImage;
+
+function base64ToImage(dir) {
+  return function(req, res, next) {
+    var raw = req.body.base64;
+    var filename = req.params.filename ? req.params.filename : 'demo.png';
+    if (!checkBase64(raw)) return next();
+    var base64 = raw.replace(/^data:image\/png;base64,/, "");
+    var abs = path.join(dir, filename);
+    fs.writeFile(abs, base64, 'base64', function(err) {
+      if (err) return next(err);
+      res.locals.image = {
+        name: filename,
+        abs: abs
+      };
+      return next();
+    });
+  }
 }
 
-module.exports = function(dir) {
-    return function(req, res, next) {
-        var raw = req.body.base64;
-        var filename = req.params.filename ? req.params.filename : 'demo.png';
-        if (checkBase64(raw)) {
-            var base64 = raw.replace(/^data:image\/png;base64,/, ""),
-                abs = path.join(dir,filename);
-            fs.writeFile(abs, base64, 'base64', function(err) {
-                if (!err) {
-                    res.locals.image = {
-                        name: filename,
-                        abs: abs
-                    }
-                    next();
-                } else {
-                    next(err);
-                }
-            });
-        } else {
-            next();
-        }
-    }
+function checkBase64(raw) {
+  return raw && typeof(raw) == 'string' && raw.match(/^data:image\/png;base64,/)
 }
